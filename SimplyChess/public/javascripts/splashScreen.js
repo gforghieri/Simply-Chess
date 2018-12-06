63d88af26a115dfa40cf298ca757cbeb44a49b99
Giuliano Forghieri <arniboy92@gmail.com>
2018-12-06 19:30:21 +0100

    Removed old files&unused depend, html doctype, const instead of var

--- SimplyChess/public/javascripts/splashScreen.js
+++ SimplyChess/public/javascripts/splashScreen.js
@@ -1,16 +1,15 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
-    console.log(1);
 }
 
 // When the user clicks on <span> (x), close the modal
