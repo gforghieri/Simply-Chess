0e36226a5ae6c0bf6fe6e8ab8a8341edc928e795
Victor Prins <victor.prins@serviceheroes.com>
2018-12-15 13:46:33 +0100

    first steps

--- SimplyChess/app.js
+++ SimplyChess/app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const http = require("http");
 const messages = require('./public/javascripts/messages.js');
-
-console.log(messages);
-
+const websocket = require('ws');
 
 const port = process.argv[2] || 3000;
 const app = express();
-
+const server = http.createServer(app);
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
-  console.log('listening on *: ' + port);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/splashScreen.html');
 });
 
+
+
+let nextGameId = 0;
+
+const wss = new websocket.Server({server});
+
+ws.gameId = nextGameId;
+
+wss.on('connection', function(ws) {
+
+
+  
+  ws.on('message', function(message) {
+
+  });
+
+});
+
+
+
+
+
+server.listen(port);
