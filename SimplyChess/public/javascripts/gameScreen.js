883daaf56cb37fc81664fbd86945c06bd036a407
Giuliano Forghieri <arniboy92@gmail.com>
2018-12-15 20:20:40 +0100

    counter, animation

---
+++ SimplyChess/public/javascripts/gameScreen.js
@@ -0,0 +1,41 @@
+//Fullscreen button
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
+
+let counter = document.getElementsByTagName('time')[0],
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+  timer();
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    counter.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+}
+function timer() {
+    setTimeout(add, 1000);
+}
+timer();
