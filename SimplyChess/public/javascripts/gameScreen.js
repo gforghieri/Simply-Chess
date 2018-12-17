d682a90c72560b6f7c39f5651328e48889819ce3
Victor Prins <victor.prins@serviceheroes.com>
2018-12-17 15:13:17 +0100

    fullscreen

--- SimplyChess/public/javascripts/gameScreen.js
+++ SimplyChess/public/javascripts/gameScreen.js
@@ -1,16 +1,21 @@
 /* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
+const elem = document.documentElement;
 
 /* View in fullscreen */
 function toggleFullScreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
+  if(document.fullscreen) {
+    document.exitFullscreen();
+  } else {
+
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
   }
 }
   const counter = document.getElementsByTagName('time')[0];
